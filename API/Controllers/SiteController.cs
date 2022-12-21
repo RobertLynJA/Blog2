7934a6bccbb5bf42d5c323fa34adadf88826fc0a
Robert Lyn <lenny4422@gmail.com>
2022-12-21 15:10:23 -0500

    Added API at / to prevent 404 errors from Azure Always On

---
+++ API/Controllers/SiteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class SiteController : ControllerBase
+    {
+        [Route("/")]
+        [HttpGet]
+        public string GetHelloMessage()
+        {
+            return "Hello, I'm working :)";
+        }
+    }
+}
