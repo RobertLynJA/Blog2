a74d08738b90b4109d28f4666a590dd6cd024d7e
Robert Lyn <lenny4422@gmail.com>
2022-12-21 15:10:23 -0500

    Added API at / to prevent 404 errors from Azure Always On

---
+++ Api.Tests/Controllers/SiteControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using API.Controllers;
+
+namespace Api.Tests.Controllers
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var controller = new SiteController();
+
+            var result = controller.GetHelloMessage();
+
+            Assert.Equal("Hello, I'm working :)", result);
+        }
+    }
+}
