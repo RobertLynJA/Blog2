7df0b8a3c1ddd8aaa1b6594af3ded7710f026ef4
Robert Lyn <lenny4422@gmail.com>
2022-12-22 16:52:39 -0500

    Add StoriesController

---
+++ Api.Tests/Controllers/StoriesControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Tests.Controllers
+{
+    public class StoriesControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
