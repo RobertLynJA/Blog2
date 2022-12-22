8e19774622256bcb06680a93168479b78f2da336
Robert Lyn <lenny4422@gmail.com>
2022-12-22 13:27:24 -0500

    Fix test, change controller to return actionresult

--- API/Controllers/SiteController.cs
+++ API/Controllers/SiteController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -8,9 +7,9 @@ namespace API.Controllers
     {
         [Route("/")]
         [HttpGet]
-        public string GetHelloMessage()
+        public ActionResult<string> GetHelloMessage()
         {
-            return "Hello, I'm working :)";
+            return Ok("Hello, I'm working :)");
         }
     }
 }
