a5a89c57844b8bce28b4a46dc8cf8b92104a6a18
Robert Lyn <lenny4422@gmail.com>
2022-12-22 22:21:11 -0500

    Add Tailwind CSS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Default React site, updates coming soon.
-        </p>
-        <p>
-          Deployed automatically from <a className="App-link" href="https://github.com/RobertLynJA/Blog2">Github</a>
-        </p>
-      </header>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div>
+        <div className="text-xl font-medium text-black">Hello from <a href="https://reactjs.org">React</a> + <a href="https://tailwindcss.com/">Tailwind</a></div>
+        <p className="text-slate-500">Deployed from <a href="https://github.com/robertlynja/blog2">Github</a></p>
+      </div>
     </div>
   );
 }
