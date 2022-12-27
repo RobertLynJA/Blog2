dbf2ae92d2a22808fc9707fb2f5c612b03eb473f
Robert Lyn <lenny4422@gmail.com>
2022-12-26 21:56:57 -0500

    Replace react project with next

--- web/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders github link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Github/i);
-  expect(linkElement).toBeInTheDocument();
-});
