cc51c77ab430ff27ab4241c105573263c64fe5af
Robert Lyn <lenny4422@gmail.com>
2022-12-30 22:38:16 -0500

    Move Layout into UI folder

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,11 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
-import Layout from '@/components/Layout'
+import "@/styles/global.css";
+import type { AppProps } from "next/app";
+import Layout from "@/components/UI/Layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
