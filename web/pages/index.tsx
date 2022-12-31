fed6ce8be017c5f2f8f588185e1486f996f45432
Robert Lyn <lenny4422@gmail.com>
2022-12-30 22:39:22 -0500

    Migrate api call out of index
    A

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,25 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import axios from 'axios';
+import Head from "next/head";
+import Image from "next/image";
+import { NextPage, GetServerSideProps } from "next";
 
-import { NextPage, GetServerSideProps } from 'next'
+import { getStoriesByDate, Story } from "../store/StoriesStore";
 
 interface Props {
   stories: Story[];
-  error: string;
+  error: string | null;
 }
 
-interface Story {
-  id: string,
-  title: string,
-  content: string,
-  publishedDate: string
-};
-
 const Home: NextPage<Props> = (props) => {
-
-  console.log(props.stories);
-
   return (
     <div>
       <Head>
@@ -29,44 +19,44 @@ const Home: NextPage<Props> = (props) => {
 
       <main>
         <h1 className="text-3xl">
-          Hello from <a href="https://nextjs.org">Next.js</a> and <a href="https://tailwindcss.com/">Tailwind</a>
+          Hello from <a href="https://nextjs.org">Next.js</a> and{" "}
+          <a href="https://tailwindcss.com/">Tailwind</a>
         </h1>
 
-        <h2>
-          Ignore random test data:
-        </h2>
+        <h2>Ignore random test data:</h2>
         <div>
-          {props.stories?.map(item => <div key={item.id}>
-            {item.content} - {(new Date(item.publishedDate).toString())}
-          </div>)}
+          {props.stories?.map((item) => (
+            <div key={item.id}>
+              {item.content} - {new Date(item.publishedDate).toString()}
+            </div>
+          ))}
         </div>
-        <div>
-          {props.error}
-        </div>
+        <div>{props.error}</div>
       </main>
 
-      <footer>
-      </footer>
+      <footer></footer>
     </div>
-  )
+  );
 };
 
-export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
-  let error: any = null;
-  let result: any;
-
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
   try {
-    result = await axios.get(process.env.API_ROOT + "/stories");
-  } catch (err: any) {
-    error = err.message;
+    return {
+      props: {
+        stories: await getStoriesByDate(0, 20),
+        error: null,
+      },
+    };
+  } catch (error: any) {
+    return {
+      props: {
+        stories: [],
+        error: error.message,
+      },
+    };
   }
-
-  return {
-    props: {
-      stories: result.data as Story[],
-      error: error
-    },
-  }
-}
+};
 
 export default Home;
