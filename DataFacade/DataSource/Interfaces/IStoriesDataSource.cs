bd0672e879822db37d8128b442aa2c8d12bb843e
Robert Lyn <lenny4422@gmail.com>
2023-01-05 22:39:59 -0500

    Remove EFCore, add CosmosDB

--- DataFacade/DataSource/Interfaces/IStoriesDataSource.cs
+++ DataFacade/DataSource/Interfaces/IStoriesDataSource.cs
@@ -5,11 +5,21 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DataFacade.DataSource.Interfaces
 {
     public interface IStoriesDataSource
     {
         ReadOnlyCollection<Story> GetStories();
+
+        IEnumerable<Story> GetStoriesByDate(int page, int numberRows);
+        IEnumerable<Story> GetStories(int year, int month);
+        Story GetStory(string storyId);
+        int GetStoryCount();
+        IEnumerable<int> GetStoryYears();
+        IEnumerable<int> GetStoryMonths(int year);
+        //Data.StoryAttachment GetAttachment(int attachmentId);
+        //IEnumerable<Data.StoryAttachment> GetAttachments(int storyId);
     }
 }
