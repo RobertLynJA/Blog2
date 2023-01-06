b95db03ba97870a9d2004bb9ba9014a8f40b1b39
Robert Lyn <lenny4422@gmail.com>
2023-01-06 14:06:58 -0500

    Add connection pooling for Cosmos

--- API/Program.cs
+++ API/Program.cs
@@ -11,18 +11,6 @@ namespace API
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
 
-            //try
-            //{
-            //    var context = services.GetRequiredService<DataFacade.BlogContext>();
-            //    context.Database.Migrate();
-            //}
-            //catch (Exception ex)
-            //{
-            //    var logger = services.GetRequiredService<ILogger<Program>>();
-            //
-            //    logger.LogError(ex, "An error occurred during DB migration");
-            //}
-
             host.Run();
         }
 
