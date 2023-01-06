174f2dc7f4091be4ced7bffbb7f96357def27569
Robert Lyn <lenny4422@gmail.com>
2023-01-05 22:39:59 -0500

    Remove EFCore, add CosmosDB

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace API
@@ -12,17 +11,17 @@ namespace API
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
 
-            try
-            {
-                var context = services.GetRequiredService<DataFacade.BlogContext>();
-                context.Database.Migrate();
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-
-                logger.LogError(ex, "An error occurred during DB migration");
-            }
+            //try
+            //{
+            //    var context = services.GetRequiredService<DataFacade.BlogContext>();
+            //    context.Database.Migrate();
+            //}
+            //catch (Exception ex)
+            //{
+            //    var logger = services.GetRequiredService<ILogger<Program>>();
+            //
+            //    logger.LogError(ex, "An error occurred during DB migration");
+            //}
 
             host.Run();
         }
--- DataFacade/DataSource/StoriesDataSource.cs
+++ DataFacade/DataSource/StoriesDataSource.cs
@@ -13,21 +13,57 @@ namespace DataFacade.DataSource
     public class StoriesDataSource : IStoriesDataSource
     {
         private readonly ILogger<StoriesDataSource> _logger;
-        private readonly BlogContext _blogContext;
-
-        public StoriesDataSource(ILogger<StoriesDataSource> logger, BlogContext blogContext)
+        private readonly string _connectionString;
+        
+        public StoriesDataSource(ILogger<StoriesDataSource> logger, string connectionString)
         {
-            _logger = logger;
-            _blogContext = blogContext;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+
+            _logger.LogInformation($"{nameof(StoriesDataSource)}()");
         }
 
         public ReadOnlyCollection<Story> GetStories()
         {
-            List<Story> stories = new();
-            stories.Add(new Story() { Content = "Hello from NextJS", ID = 1, PublishedDate = DateTime.UtcNow, Title = "First" });
-            stories.Add(new Story() { Content = "Hello from TailWind", ID = 2, PublishedDate = DateTime.UtcNow, Title = "Second" });
+            List<Story> stories = new()
+            {
+                new Story() { Content = "Hello from NextJS", ID = 1, PublishedDate = DateTime.UtcNow, Title = "First" },
+                new Story() { Content = "Hello from TailWind", ID = 2, PublishedDate = DateTime.UtcNow, Title = "Second" }
+            };
+
+            _logger.LogInformation($"{nameof(StoriesDataSource)}:{nameof(GetStories)}()");
 
             return new ReadOnlyCollection<Story>(stories);
+        }
+
+        public IEnumerable<Story> GetStories(int year, int month)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Story> GetStoriesByDate(int page, int numberRows)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Story GetStory(string storyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetStoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetStoryMonths(int year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetStoryYears()
+        {
+            throw new NotImplementedException();
         }
     }
 }
