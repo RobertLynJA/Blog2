f0a176f29ed7478ab780fdc03c876069e7661754
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-01-17 23:04:23 -0500

    Added some styling using font weight and padding.

--- web/components/Stories/StorySummary.tsx
+++ web/components/Stories/StorySummary.tsx
@@ -6,24 +6,25 @@ import StoryContent from "./StoryContent";
 
 interface Props {
   story: Story;
+  className?: string;
 }
 
 const StorySummary: FunctionComponent<Props> = (props) => {
   return (
-    <div>
+    <div className={props.className}>
       <div>
         <Link
           href={`/stories/${encodeURIComponent(props.story.id)}`}
-          className="text-xl"
+          className="text-xl font-medium"
         >
           {props.story.title}
         </Link>
       </div>
-      <StoryContent
+      <StoryContent className="pt-4 pb-2"
         content={props.story.content}
         encoding={props.story.encoding}
       />
-      <div className="text-sm">
+      <div className="text-sm font-light">
         {new Date(props.story.publishedDate).toUTCString()}
       </div>
     </div>
--- web/components/UI/Layout.tsx
+++ web/components/UI/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ReactNode, FunctionComponent } from "react";
 import CookieConsent from "react-cookie-consent";
 
@@ -7,12 +8,18 @@ interface Props {
 
 const Layout: FunctionComponent<Props> = (props) => {
   return (
-    <>
+    <div className="container mx-auto px-8 py-8">
+      <Head>
+        <title>RobertLynJA.com</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-3xl pb-8">Welcome to RobertLynJA.com</h1>
       {props.children}
       <CookieConsent>
         This website uses cookies to enhance the user experience.
       </CookieConsent>
-    </>
+    </div>
   );
 };
 
