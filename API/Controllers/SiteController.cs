6374c518115ab02b4d3b9750d2b99ef7b1536dbc
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-02-01 18:09:56 -0500

    Add Auth0 to API

--- API/Controllers/SiteController.cs
+++ API/Controllers/SiteController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -7,9 +10,32 @@ namespace API.Controllers;
 [ApiController]
 public class SiteController : ControllerBase
 {
+    private readonly ILogger<StoriesController> _logger;
+
+    public SiteController(ILogger<StoriesController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
     public string GetHelloMessage()
     {
         return "Hello, I'm working :)";
+    }
+
+    [Route("testadmin")]
+    [Authorize(Policy="Admin")]
+    [HttpGet]
+    public string TestAdmin()
+    {
+        return "Authorized Admin";
+    }
+
+    [Route("testuser")]
+    [Authorize]
+    [HttpGet]
+    public string TestUser()
+    {
+        return "Authorized User";
     }
 }
