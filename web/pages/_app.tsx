7f9c5023f73734133f36b41a13e462faed7e3711
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-02-01 17:00:02 -0500

    Add Auth0 login to web

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 import Layout from "@/components/UI/Layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
   );
 }
