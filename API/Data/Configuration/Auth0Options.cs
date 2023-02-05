78689b06c0d5ef5d3ea50d2fc171f36bf2b5f42c
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-02-04 21:49:59 -0500

    Add class to get user info from Auth0

--- API/Data/Configuration/Auth0Options.cs
+++ API/Data/Configuration/Auth0Options.cs
@@ -2,6 +2,11 @@
 
 public sealed class Auth0Options
 {
-    public string Authority { get; set; }
-    public string Audience { get; set; }
+    public string? Authority { get; set; }
+    public string? Audience { get; set; }
+    public string? APIClientID { get; set; }
+    public string? APIClientSecret { get; set; }
+    public string? APIAudience { get; set; }
+    public string? APIGrantType { get; set; }
+    public string? APIRootURL { get; set; }
 }
---
+++ API/User/Interfaces/IUserManager.cs
@@ -0,0 +1,8 @@
+﻿using API.Data.Auth0;
+
+namespace API.User.Interfaces;
+
+public interface IUserManager
+{
+    Task<Auth0User?> GetCurrentUserAsync(CancellationToken cancellationToken);
+}
