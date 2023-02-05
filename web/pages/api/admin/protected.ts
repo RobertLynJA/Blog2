789d83248b1f67d6e31a60167d401b85fdf469a0
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-02-05 13:55:18 -0500

    Make calls to protected API

--- web/pages/api/admin/protected.ts
+++ web/pages/api/admin/protected.ts
@@ -1,12 +1,17 @@
 import { withApiAuthRequired, getSession, getAccessToken } from '@auth0/nextjs-auth0';
 
+import { testUser, testAdmin } from 'store/StoriesStore';
+
 export default withApiAuthRequired(async function myApiRoute(req, res) {
   const { accessToken } = await getAccessToken(req, res, {
     scopes: ['write:stories']
   });
   
-  console.log(accessToken);
+  console.log("Access Token: " + accessToken);
+
+  const getUser = await testUser(accessToken!);
+  const getAdmin = await testAdmin(accessToken!);
 
   const { user } = await getSession(req, res);
-  res.json({ protected: 'My Secret', id: user.sub });
+  res.json({ protected: 'My Secret', id: user.sub, user: getUser, admin: getAdmin});
 });
--- web/store/StoriesStore.ts
+++ web/store/StoriesStore.ts
@@ -46,11 +46,34 @@ export const getStory = async (id: string) => {
   }
 };
 
-export const testUser = async () => {
-  const url = `${API_ROOT}/testuser}`;
+export const testUser = async (accessToken: string) => {
+  const url = `${API_ROOT}/testuser`;
 
   try {
-    const { data } = await axios.get<string>(url);
+    const { data } = await axios.get<string>(url, 
+      { headers: {"Authorization" : `Bearer ${accessToken}`} });
+    return data;
+  } catch (error: any) {
+
+    console.log(error);
+
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 404) {
+        return null;
+      }
+    }
+
+    ErrorLogger(url, error);
+    throw new Error(error.message);
+  }
+};
+
+export const testAdmin = async (accessToken: string) => {
+  const url = `${API_ROOT}/testadmin`;
+
+  try {
+    const { data } = await axios.get<string>(url, 
+      { headers: {"Authorization" : `Bearer ${accessToken}`} });
     return data;
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
