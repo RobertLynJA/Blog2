80fd5ec23b11da23d7cafb9f73cd7476660b6141
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-02-04 21:47:22 -0500

    Add Auth0 API classes

---
+++ API/Data/Auth0/Auth0Token.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Data.Auth0;
+
+public class Auth0Token
+{
+    [JsonPropertyName("access_token")]
+    public string? AccessToken { get; set; }
+
+    [JsonPropertyName("scope")]
+    public string? Scope { get; set; }
+
+    [JsonPropertyName("expires_in")]
+    public int? ExpiresIn { get; set; }
+
+    [JsonPropertyName("token_type")]
+    public string? TokenType { get; set; }
+}
