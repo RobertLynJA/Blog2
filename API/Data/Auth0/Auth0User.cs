c28a9780783d3a67a8663384cc14a7092743d952
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2023-02-04 21:47:22 -0500

    Add Auth0 API classes

---
+++ API/Data/Auth0/Auth0User.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Data.Auth0;
+
+public class Auth0User
+{
+    [JsonPropertyName("created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
+
+    [JsonPropertyName("email_verified")]
+    public bool? EmailVerified { get; set; }
+
+    [JsonPropertyName("identities")]
+    public Identity[]? Identities { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("nickname")]
+    public string? Nickname { get; set; }
+
+    [JsonPropertyName("picture")]
+    public string? Picture { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [JsonPropertyName("user_id")]
+    public string? UserID { get; set; }
+
+    [JsonPropertyName("last_ip")]
+    public string? LastIP { get; set; }
+
+    [JsonPropertyName("last_login")]
+    public DateTime? LastLogin { get; set; }
+
+    [JsonPropertyName("logins_count")]
+    public int? LoginsCount { get; set; }
+
+    public class Identity
+    {
+        [JsonPropertyName("connection")]
+        public string? Connection { get; set; }
+
+        [JsonPropertyName("provider")]
+        public string? Provider { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string? UserID { get; set; }
+
+        [JsonPropertyName("isSocial")]
+        public bool? IsSocial { get; set; }
+    }
+}
