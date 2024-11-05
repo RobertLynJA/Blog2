a0a9d687d15a2ec4d0eeee6b7a2418b6aaa2581c
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2024-11-05 15:44:01 -0500

    Remove AutoMapper dependency

--- API/Controllers/StoriesController.cs
+++ API/Controllers/StoriesController.cs
@@ -1,68 +1,70 @@
-﻿using AutoMapper;
+﻿using API.Models.Stories;
 using DataFacade.Commands.Stories;
 using MediatR;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StoriesController(ILogger<StoriesController> logger, IMediator mediator) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StoriesController(ILogger<StoriesController> logger, IMapper mapper, IMediator mediator) : ControllerBase
+    private readonly ILogger<StoriesController> _logger = logger;
+    private readonly IMediator _mediator = mediator;
+
+    // GET <StoriesController>
+    [HttpGet()]
+    [ResponseCache(CacheProfileName = "10MinutesPublic")]
+    public async Task<ActionResult<List<Models.Stories.Story>>> Get(CancellationToken cancellationToken)
     {
-        private readonly ILogger<StoriesController> _logger = logger;
-        private readonly IMapper _mapper = mapper;
-        private readonly IMediator _mediator = mediator;
-
-        // GET <StoriesController>
-        [HttpGet()]
-        [ResponseCache(CacheProfileName = "10MinutesPublic")]
-        public async Task<ActionResult<List<Models.Stories.Story>>> Get(CancellationToken cancellationToken)
+        try
         {
-            try
-            {
-                var stories = await _mediator.Send(new GetStoriesByDateCommand(0, 10), cancellationToken);
-                var result = _mapper.Map<IEnumerable<Models.Stories.Story>>(stories).ToList();
+            var stories = await _mediator.Send(new GetStoriesByDateCommand(0, 10), cancellationToken);
+            var result = stories.Select(s => Story.FromDAO(s)).ToList();
 
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "URL: {URL}", Request.GetDisplayUrl());
-                throw;
-            }
+            _logger.LogInformation("URL: {URL}", Request.GetDisplayUrl());
+
+            return Ok(result);
         }
-
-        // GET <StoriesController>/5
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ResponseCache(CacheProfileName = "10MinutesPublic")]
-        public async Task<ActionResult<Models.Stories.Story>> Get(string id, CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            try
+            _logger.LogError(ex, "URL: {URL}", Request.GetDisplayUrl());
+            throw;
+        }
+    }
+
+    // GET <StoriesController>/5
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ResponseCache(CacheProfileName = "10MinutesPublic")]
+    public async Task<ActionResult<Models.Stories.Story>> Get(string id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(id))
             {
-                if (string.IsNullOrEmpty(id))
-                {
-                    return NotFound();
-                }
-
-                var story = await _mediator.Send(new GetStoryByIDCommand(id), cancellationToken);
-
-                if (story == null)
-                {
-                    return NotFound();
-                }
-
-                var result = _mapper.Map<Models.Stories.Story>(story);
-
-                return Ok(result);
+                return NotFound();
             }
-            catch (Exception ex)
+
+            var story = await _mediator.Send(new GetStoryByIDCommand(id), cancellationToken);
+
+            if (story == null)
             {
-                _logger.LogError(ex, "URL: {URL}", Request.GetDisplayUrl());
-                throw;
+                return NotFound();
             }
+
+            var result = Story.FromDAO(story);
+
+            _logger.LogInformation("URL: {URL}", Request.GetDisplayUrl());
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "URL: {URL}", Request.GetDisplayUrl());
+            throw;
         }
     }
 }
