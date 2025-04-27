c98ccf4de47c13d2ac35175db58838eca63271f6
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-04-27 16:45:27 -0400

    Add security headers

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'no-referrer-when-downgrade',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
